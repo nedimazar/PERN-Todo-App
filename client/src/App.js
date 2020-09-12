a5fc2ae621832307d294d0d15756fd0c549d8684
nedimazar <nedimazar@hotmail.com>
2020-09-12 22:32:18 +0300

    Structure

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React, {Fragment} from 'react';
+import './App.css';
+
+//Components
+import InputTodo from "./components/inputTodo";
+import ListTodos from "./components/listTodos";
+
+function App() {
+  return (
+   <Fragment>
+     <div className="container">
+        <InputTodo />
+        <ListTodos />
+     </div>
+   </Fragment>
+    
+  );
+}
+
+export default App;
